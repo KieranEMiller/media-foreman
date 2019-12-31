f9df5196d993d3d546c5eb611c7a6291226c7c3e
kieranemiller <kieranemiller@gmail.com>
2019-12-31 11:53:04 -0500

    make each analysis implement its base classes and abstract methods

--- src/kem/mediaforeman/analyses/collection_analysis_same_artist.py
+++ src/kem/mediaforeman/analyses/collection_analysis_same_artist.py
@@ -3,15 +3,14 @@ Created on Dec 30, 2019
 
 @author: kieranemiller
 '''
+from kem.mediaforeman.analyses.collection_analysis_base import CollectionAnalysisBase
+from kem.mediaforeman.analyses.analysis_type import AnalysisType
 
-class CollectionAnalysisSameArtist(object):
-    '''
-    classdocs
-    '''
-
+class CollectionAnalysisSameArtist(CollectionAnalysisBase):
 
     def __init__(self, params):
-        '''
-        Constructor
-        '''
+        pass
+    
+    def GetAnalysisType(self):
+        return AnalysisType.CollectionSameArtist
         
--- src/kem/mediaforeman/analyses/file_analysis_complete_audio_metadata.py
+++ src/kem/mediaforeman/analyses/file_analysis_complete_audio_metadata.py
@@ -3,10 +3,16 @@ Created on Dec 30, 2019
 
 @author: kieranemiller
 '''
+from kem.mediaforeman.analyses.file_analysis_base import FileAnalysisBase
 
-class FileAnalysisCompleteAudioMetadata(object):
+class FileAnalysisCompleteAudioMetadata(FileAnalysisBase):
 
     def __init__(self, params):
         pass
         
-        
+    def GetAnalysisType(self):
+        pass
+
+    def RunAnalysisOnFile(self, mediaFile):
+        pass
+    
