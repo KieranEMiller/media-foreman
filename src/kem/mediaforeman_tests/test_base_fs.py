61c0a50e4b9c6f6b2602d389043f0f6e9bcd65d5
kieranemiller <kieranemiller@gmail.com>
2020-01-01 17:56:59 -0500

    add new helper methods CreateSubDirectory and CopySampleMp3ToDir

--- src/kem/mediaforeman_tests/test_base_fs.py
+++ src/kem/mediaforeman_tests/test_base_fs.py
@@ -6,16 +6,28 @@ Created on Dec 29, 2019
 import unittest
 import shutil
 import tempfile
+import os
 
 class TestBaseFs(unittest.TestCase):
 
     def setUp(self):
-        self.testdir = tempfile.mkdtemp()
-        print("BaseFsTests: created temp dir {}".format(self.testdir))
+        self._testDir = tempfile.mkdtemp()
+        return self._testDir
 
     def tearDown(self):
-        shutil.rmtree(self.testdir)
-        print("BaseFsTests: cleaned temp dir {}".format(self.testdir))
+        shutil.rmtree(self._testDir)
+
+    def CreateSubDirectory(self, name):
+        newDir = os.path.join(self._testDir, name)
+        os.mkdir(newDir)
+        return newDir
+    
+    def CopySampleMp3ToDir(self, dir):
+        pathToSample = "./assets/sample_mp3.mp3"
+        dest = os.path.join(self._testDir, os.path.basename(pathToSample));
+        shutil.copy(os.path.abspath(pathToSample), dest)
+        return dest
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
