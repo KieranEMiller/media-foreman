f3d50e501aad5d587786d6e91d1c1ac108c174d1
kieranemiller <kieranemiller@gmail.com>
2020-01-01 17:53:13 -0500

    update to inherit from FileAnalysisBase and implement abstract methods

--- src/kem/mediaforeman/analyses/file_analysis_media_file_type.py
+++ src/kem/mediaforeman/analyses/file_analysis_media_file_type.py
@@ -3,9 +3,16 @@ Created on Dec 30, 2019
 
 @author: kieranemiller
 '''
+from kem.mediaforeman.analyses.file_analysis_base import FileAnalysisBase
 
-class FileAnalysisMediaFileType(object):
+class FileAnalysisMediaFileType(FileAnalysisBase):
 
     def __init__(self, params):
         pass
         
+    def GetAnalysisType(self):
+        pass
+
+    def RunAnalysisOnFile(self, mediaFile):
+        pass
+    
