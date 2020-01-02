970f64eb367bd3d6ca4b48a33cd481a147cbc392
kieranemiller <kieranemiller@gmail.com>
2020-01-01 19:13:15 -0500

    remove IsMedia method as that will be handled by one of the analysis classes

--- src/kem/mediaforeman/media_file.py
+++ src/kem/mediaforeman/media_file.py
@@ -6,12 +6,6 @@ class MediaFile(MediaBase):
     def __init__(self, path):
         super(MediaFile, self).__init__(path)
         
-    def IsMedia(self):
-        '''if the extension is one of a set of types
-        //currently just supporting audio'''
-        audioTypes = ['mp3', 'wav', 'ogg', 'mp4', 'flac']
-        return True
-
     def ExtractProperties(self):
         '''
         //get all properties as a dictionary
