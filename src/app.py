4a930c12452b580d4f653417dfb42bb4b0643aff
kieranemiller <kieranemiller@gmail.com>
2020-01-01 19:48:53 -0500

    update package name/path for the ConsoleApp class

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ Created on Dec 29, 2019
 
 @author: kieranemiller
 '''
-from kem.mediaforeman.consoleapp import ConsoleApp
+from kem.mediaforeman.console_app import ConsoleApp
 
 if __name__ == '__main__':
     print("media foreman: manage and maintain media files and metadata\n")
