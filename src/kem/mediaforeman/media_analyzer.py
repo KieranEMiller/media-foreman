7519e513df9b32c47ba668c1c51c8b19ff2b0c21
kieranemiller <kieranemiller@gmail.com>
2020-01-02 21:05:00 -0500

    1) add a default set of analyses to run when none are specified 2) implement basic iteration over configured analysis types

--- src/kem/mediaforeman/media_analyzer.py
+++ src/kem/mediaforeman/media_analyzer.py
@@ -3,18 +3,36 @@ Created on Dec 31, 2019
 
 @author: kieranemiller
 '''
-from kem.mediaforeman.analyses.file_analysis_media_file_type import FileAnalysisMediaFileType
+import datetime
+
 from kem.mediaforeman.media_analyzer_result import MediaAnalyzerResult
+from kem.mediaforeman.analyses.file_analysis_media_file_type import FileAnalysisMediaFileType
+from kem.mediaforeman.analyses.collection_analysis_same_artist import CollectionAnalysisSameArtist
 
 class MediaAnalyzer(object):
 
-    def __init__(self, media):
+    def __init__(self, media, analyses = None):
         self._media = media
+        self._analyses = analyses
         
-    def Analyze(self, summaryOnly):
+        if(analyses is None):
+            self._analyses = [FileAnalysisMediaFileType()]
         
-        fileAnalyses = [FileAnalysisMediaFileType]
+    def Analyze(self, summaryOnly = True):
+        
+        startTime = datetime.datetime.now()
+
         result = MediaAnalyzerResult()
+        result.AnalysesRun.extend(self._analyses)
         
+        '''TODO: decide if its better to iterate over media or by analysis type?'''
         for media in self._media:
-            print("analyzing media {}".format(media))
+            print("analyzing media: type {}, base path {}".format(type(media), media.BasePath))
+
+            for analysis in self._analyses:
+                print("running analysis {}".format(type(analysis)))
+                analysisResults = analysis.RunAnalysis(media = media)
+                result.AnalysisResults.append(analysisResults)
+
+        result.ElapsedInMicroSecs = (datetime.datetime.now() - startTime).microseconds
+        return result
--- src/kem/mediaforeman_tests/test_media_analyzer.py
+++ src/kem/mediaforeman_tests/test_media_analyzer.py
@@ -5,11 +5,21 @@ Created on Jan 1, 2020
 '''
 import unittest
 from kem.mediaforeman_tests.test_base_fs import TestBaseFs
+from kem.mediaforeman.media_analyzer import MediaAnalyzer
+from kem.mediaforeman.media_file import MediaFile
 
 class TestMediaAnalyzer(TestBaseFs):
 
-    def test_media_analyzer_runs_(self):
-        pass
+    def test_media_analyzer_runs_default_analysis_if_none_specified(self):
+        sampleFile = self.CopySampleMp3ToDir(self._testDir)
+        media = MediaFile(sampleFile)
+
+        analyzer = MediaAnalyzer([media])
+        results = analyzer.Analyze()
+        self.assertIsNotNone(results)
+        
+        self.assertGreater(len(results.AnalysesRun), 0)
+        self.assertGreater(len(results.AnalysisResults), 0)
 
 
 if __name__ == "__main__":
