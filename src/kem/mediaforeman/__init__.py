57f065fb09070a44e2bc3e7676ce7248784322fe
kieranemiller <kieranemiller@gmail.com>
2020-01-07 22:59:17 -0500

    move the logging init to the base package, kem.mediaforeman.__init__.py.  this allows the logging to be enabled and functional even when running unit tests
    as part of this, change how the log path is calculated so that the log file appears in the root src folder.  see code comments for details on how this is accomplished but essentially we walk up 2 levels from the __init__ file where the logging configuration is done

---
+++ src/kem/mediaforeman/__init__.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import inspect
+import logging
+import logging.config
+
+'''get the name of this file; have to use this method as opposed
+to the __file__ property since this is running as main and 
+not a class or module'''
+currentDir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+
+'''go up 2 directories from this init script, which is the base folder 
+for the app right under src'''
+logPath = os.path.abspath(os.path.join(currentDir, "..", "..", "mediaforeman.log"))
+
+print("logpath: {}".format(logPath))
+
+config = {
+    'version': 1,
+    'formatters': {
+        # Modify log message format here or replace with your custom formatter class
+        'my_formatter': {
+            'format': '(%(process)d) %(asctime)s %(name)s (line %(lineno)s) | %(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'console_stderr': {
+            'class': 'logging.StreamHandler',
+            'level': 'ERROR',
+            'formatter': 'my_formatter',
+            'stream': sys.stderr
+        },
+        'console_stdout': {
+            'class': 'logging.StreamHandler',
+            'level': 'WARN',
+            'formatter': 'my_formatter',
+            'stream': sys.stdout
+        },
+        'file': {
+            # Sends all log messages to a file
+            'class': 'logging.FileHandler',
+            'level': "INFO",
+            'formatter': 'my_formatter',
+            'filename': logPath,
+            'encoding': 'utf8'
+        }
+    },
+    'root': {
+        # In general, this should be kept at 'NOTSET'.
+        # Otherwise it would interfere with the log levels set for each handler.
+        'level': 'NOTSET',
+        'handlers': ['console_stderr', 'console_stdout', 'file']
+    },
+}
+
+logging.config.dictConfig(config)
