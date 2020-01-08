fc5929e2b4513da0e60eee4e7abc43924ae673c8
kieranemiller <kieranemiller@gmail.com>
2020-01-07 23:02:27 -0500

    add multiple overloads to CopyMp3File by setting defaults on the parameters; this allows it to be more easily and conveniently used in unit tests eliminating the need to pass the test directory path every time

--- src/kem/mediaforeman_tests/test_base_fs.py
+++ src/kem/mediaforeman_tests/test_base_fs.py
@@ -23,14 +23,19 @@ class TestBaseFs(unittest.TestCase):
         os.mkdir(newDir)
         return newDir
 
-    def CopySampleMp3WithImgToDir(self, dir):
-        return self.CopySampleMp3ToDir(dir, "./assets/sample_mp3_with_img.mp3")
+    def CopySampleMp3WithImgToDir(self, testDir = None):
+        return self.CopySampleMp3ToDir(testDir, "./assets/sample_mp3_with_img.mp3")
     
-    def CopySampleMp3ToDir(self, dir, file="./assets/sample_mp3.mp3"):
+    def CopySampleMp3ToDir(self, testDir = None, file = "./assets/sample_mp3.mp3"):
+        if(testDir is None):
+            testDir = self._testDir
+            
         pathToSample = os.path.join(os.path.dirname(os.path.realpath(__file__)), file)
-        dest = os.path.join(self._testDir, "{}.mp3".format(str(uuid.uuid1())));
+        dest = os.path.join(testDir, "{}.mp3".format(str(uuid.uuid1())));
         shutil.copy(os.path.abspath(pathToSample), dest)
-        '''print("copying sample mp3 from {} to {}".format(os.path.abspath(pathToSample), dest))'''
+        
+        #print("copying sample mp3 from {} to {}".format(os.path.abspath(pathToSample), dest))
+        
         return dest
     
         
