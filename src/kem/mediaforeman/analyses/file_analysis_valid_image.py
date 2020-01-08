94d8b6f8c13b486783d5fe0860837cc096df0b5a
kieranemiller <kieranemiller@gmail.com>
2020-01-07 23:06:16 -0500

    implement the valid image analysis class with test coverage

--- src/kem/mediaforeman/analyses/file_analysis_valid_image.py
+++ src/kem/mediaforeman/analyses/file_analysis_valid_image.py
@@ -5,15 +5,33 @@ Created on Dec 30, 2019
 '''
 from kem.mediaforeman.analyses.file_analysis_base import FileAnalysisBase
 from kem.mediaforeman.analyses.analysis_type import AnalysisType
+from kem.mediaforeman.analyses.analysis_issue_property_invalid import AnalysisIssuePropertyInvalid
+from kem.mediaforeman.analyses.analysis_issue_threshold_not_met import AnalysisIssueThresholdNotMet
 
 class FileAnalysisValidImage(FileAnalysisBase):
 
     def __init__(self, params):
-        pass
+        self._requiredXandYPixelLength = 256
         
     def GetAnalysisType(self):
         return AnalysisType.FileValidImage
 
     def RunAnalysisOnFile(self, mediaFile):
-        pass
-    
+        results = []
+        
+        if(mediaFile.CoverImgExists == False):
+            results.append(AnalysisIssuePropertyInvalid(
+                "Cover Image Exists", True, mediaFile.CoverImgExists
+            ))
+
+        if(mediaFile.CoverImgX < self._requiredXandYPixelLength or mediaFile.CoverImgX <= 0):
+            results.append(AnalysisIssueThresholdNotMet(
+                "Cover Image X Dimensions", self._requiredXandYPixelLength, mediaFile.CoverImgX
+            ))
+            
+        if(mediaFile.CoverImgY < self._requiredXandYPixelLength or mediaFile.CoverImgY <= 0):
+            results.append(AnalysisIssueThresholdNotMet(
+                "Cover Image Y Dimensions", self._requiredXandYPixelLength, mediaFile.CoverImgY
+            ))
+
+        return results
