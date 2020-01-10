769c67db1fb28d44accdfe679579ce2bee8cffde
kieranemiller <kieranemiller@gmail.com>
2020-01-10 07:46:01 -0500

    update tests to use the modified way to copy sample mp3 images to the testing directory

--- src/kem/mediaforeman_tests/analyses_tests/test_file_analysis_valid_image.py
+++ src/kem/mediaforeman_tests/analyses_tests/test_file_analysis_valid_image.py
@@ -6,27 +6,30 @@ from kem.mediaforeman.media_file import MediaFile
 from kem.mediaforeman.analyses.analysis_type import AnalysisType
 from kem.mediaforeman.analyses.analysis_issue_threshold_not_met import AnalysisIssueThresholdNotMet
 
+SAMPLE_MP3_BELOW_SIZE_REQS = "./assets/sample_mp3_with_img_below_size_minimums.mp3"
+SAMPLE_MP3_MEETS_SIZE_REQS = "./assets/sample_mp3_with_img_meets_size_reqs_275x275.mp3"
+
 class TestFileAnalysisValidImage(TestBaseFs):
 
     def test_returns_no_issues_when_all_properties_met(self):
-        samplePath = self.CopySampleMp3WithImgToDir(useSampleWithMinImgReqs=True)
+        samplePath = self.CopySampleMp3ToDir(testFile = SAMPLE_MP3_MEETS_SIZE_REQS)
         media = MediaFile(samplePath)
-        imgAnalysis = FileAnalysisValidImage(media)
-        
         self.assertTrue(media.CoverImgExists)
         
+        imgAnalysis = FileAnalysisValidImage()
         result = imgAnalysis.RunAnalysis(media)
+        
         self.assertFalse(result.HasIssues)
         self.assertEqual(len(result.IssuesFound), 0)
         
     def test_returns_threshold_not_met_when_size_of_coverart_does_not_meet_min(self):
-        samplePath = self.CopySampleMp3WithImgToDir()
+        samplePath = self.CopySampleMp3ToDir(testFile = SAMPLE_MP3_BELOW_SIZE_REQS)
         media = MediaFile(samplePath)
-        imgAnalysis = FileAnalysisValidImage(media)
-        
         self.assertTrue(media.CoverImgExists)
         
+        imgAnalysis = FileAnalysisValidImage()
         result = imgAnalysis.RunAnalysis(media)
+        
         self.assertTrue(result.HasIssues)
         self.assertGreater(len(result.IssuesFound), 0)
         self.assertIsInstance(result.IssuesFound[0], AnalysisIssueThresholdNotMet)
@@ -35,11 +38,11 @@ class TestFileAnalysisValidImage(TestBaseFs):
     def test_returns_property_invalid_issue_when_no_image(self):
         samplePath = self.CopySampleMp3ToDir()
         media = MediaFile(samplePath)
-        imgAnalysis = FileAnalysisValidImage(media)
-        
         self.assertFalse(media.CoverImgExists)
         
+        imgAnalysis = FileAnalysisValidImage()
         result = imgAnalysis.RunAnalysis(media)
+        
         self.assertTrue(result.AnalysisType == AnalysisType.FileValidImage)
         self.assertGreater(result.ElapsedInMicroSecs, -1)
         self.assertTrue(result.HasIssues)
