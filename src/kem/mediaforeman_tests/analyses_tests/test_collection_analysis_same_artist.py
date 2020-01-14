e0a302ec9d7e360720f309c8c4a1dd1180245c75
kieranemiller <kieranemiller@gmail.com>
2020-01-14 07:48:46 -0500

    expand tests for the same artist collection analysis to be more comprehensive

--- src/kem/mediaforeman_tests/analyses_tests/test_collection_analysis_same_artist.py
+++ src/kem/mediaforeman_tests/analyses_tests/test_collection_analysis_same_artist.py
@@ -2,8 +2,23 @@ import unittest
 from kem.mediaforeman_tests.test_base_fs import TestBaseFs
 from kem.mediaforeman.analyses.collection_analysis_same_artist import CollectionAnalysisSameArtist
 from kem.mediaforeman.media_file import MediaFile
+from kem.mediaforeman.media_collection import MediaCollection
+from kem.mediaforeman.analyses.analysis_type import AnalysisType
+from kem.mediaforeman.analyses.analysis_issue_property_invalid import AnalysisIssuePropertyInvalid
 
 class Test(TestBaseFs):
+
+    def create_sample_mp3_with_artist_name(self, name):
+        sample = MediaFile(path = None)
+        sample.AlbumArtist = name
+        return sample
+
+    def create_file_system_sample_mp3_with_artist_name(self, dir = None, artistName = ""):
+        path = self.CopySampleMp3ToDir(testDir = dir)
+        sample = MediaFile(path)
+        sample.AlbumArtist = artistName
+        sample.SaveMetadata()
+        return path
 
     def test_get_highest_count_key_basic(self):
         dict = {
@@ -31,10 +46,6 @@ class Test(TestBaseFs):
         self.assertEqual(artistName, "name1")
         self.assertEqual(artistCount, 0)
         
-    def create_sample_mp3_with_artist_name(self, name):
-        sample = MediaFile(path = None)
-        sample.AlbumArtist = name
-        return sample
     
     def test_compute_unique_artist_names_mixed_counts_basic(self):
         mediaFiles = [
@@ -104,6 +115,25 @@ class Test(TestBaseFs):
         
         self.assertEqual(likelyArtist, "artist1")
         
+    def test_full_analysis_all_same_artist_flat_dir_structure_returns_no_issues(self):
+        rootTestDir = self.CreateSubDirectory("tmp")
+        self.create_file_system_sample_mp3_with_artist_name(rootTestDir, "artist1")
+        self.create_file_system_sample_mp3_with_artist_name(rootTestDir, "artist2")
+        self.create_file_system_sample_mp3_with_artist_name(rootTestDir, "artist2")
+        self.create_file_system_sample_mp3_with_artist_name(rootTestDir, "artist2")
+        self.create_file_system_sample_mp3_with_artist_name(rootTestDir, "artist1")
+        
+        coll = MediaCollection(rootTestDir)
+        analysis = CollectionAnalysisSameArtist()
+        result = analysis.RunAnalysis(coll)
+        
+        self.assertEqual(result.AnalysisType, AnalysisType.CollectionSameArtist)
+        self.assertTrue(result.HasIssues)
+        self.assertEqual(len(result.IssuesFound), 2)
+        
+        for issue in result.IssuesFound:
+            self.assertIsInstance(issue, AnalysisIssuePropertyInvalid)
+            self.assertEqual(issue.ActualVal, "artist1")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
