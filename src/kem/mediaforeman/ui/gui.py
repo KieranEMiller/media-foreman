6e1b80e607b382b1498dfd35b1f1eba6f051e258
kieranemiller <kieranemiller@gmail.com>
2020-01-19 19:44:08 -0500

    1) update the gui to only show 3 tabs: all analyses, file analyses, and collection analyses as one tab for each analysis is a bit much 2) break up the tab creation for readibility

--- src/kem/mediaforeman/ui/gui.py
+++ src/kem/mediaforeman/ui/gui.py
@@ -14,14 +14,14 @@ class GuiApp(object):
         
         '''the root container for all window controls'''
         self._rootFrame = None
-        self._resultsTabsByAnalysisType = None
+        self._resultsByAnalysisType = None
         
     def Run(self):
 
         self._rootFrame = self.SetupRootFrame()
         
         self.SetupConfigSection()
-        self._resultsTabsByAnalysisType = self.SetupResultsTabs()
+        self._resultsByAnalysisType = self.SetupResultsTabs()
         
         self._window.mainloop() 
         
@@ -55,25 +55,32 @@ class GuiApp(object):
         Grid.columnconfigure(frame, 1, weight=1)
 
         tabControl = ttk.Notebook(frame)
+        tabControl.pack(expand=1, fill='both')
         
         tabsByAnalysisType = {}
-        for analysisType in AnalysisType:
+        tabsByAnalysisType["AllAnalyses"] = self.SetupResultsTab(tabControl, "AllAnalyses")
+        tabsByAnalysisType["FileAnalyses"] = self.SetupResultsTab(tabControl, "FileAnalyses")
+        tabsByAnalysisType["CollectionAnalyses"] = self.SetupResultsTab(tabControl, "CollectionAnalyses")
+            
+        return tabsByAnalysisType
+    
+    def SetupResultsTab(self, tabControl, tabName):
             tabFrame = ttk.Frame(tabControl)
             Grid.columnconfigure(tabFrame, 1, weight=1)
             Grid.rowconfigure(tabFrame, 1, weight=1)
-            tabControl.add(tabFrame, text=analysisType.name)
+            tabControl.add(tabFrame, text=tabName)
             
-            lbl1 = Label(tabFrame, text=analysisType.name)
+            lbl1 = Label(tabFrame, text=tabName)
             lbl1.grid(column=0, row=0)
             
             tree = ttk.Treeview(tabFrame, columns=('FileName', 'Path', 'ParentDirectory'))
 
+            '''setup a vertical scrollbar'''
             vsb = ttk.Scrollbar(tabFrame, orient="vertical")
             vsb.configure(command=tree.yview)
             vsb.grid(row=0, rowspan=2, column=2, sticky=N+S)
             tree.configure(yscrollcommand=vsb.set)
 
-            
             tree.heading('#0', text='AnalysisType')
             tree.heading('#1', text='FileName')
             tree.heading('#2', text='Path')
@@ -84,14 +91,12 @@ class GuiApp(object):
             tree.column('#3', stretch=tkinter.YES)
             tree.grid(row=1, columnspan=2, sticky='nsew')
             
+            return tabFrame
+        
+            ''' insertion sample
             for i in range(50):
                 parent = tree.insert('', 'end', text='item num ' + str(i), values=('qwer', 'qwer', 'qwer'))
                 for z in range(5):
                     tree.insert(parent, 'end', text='item num ' + str(z), values=('qwer', 'qwer', 'qwer'))
             
-            tabsByAnalysisType[analysisType] = tabFrame
-            
-        tabControl.pack(expand=1, fill='both')
-        return tabControl
-    
-    
+            '''
