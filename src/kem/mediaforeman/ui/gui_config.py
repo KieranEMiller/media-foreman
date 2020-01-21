f3ba786353a1579d68fccbfac56333a3429a3764
kieranemiller <kieranemiller@gmail.com>
2020-01-21 08:00:33 -0500

    when the configuration window is shown, offset it by a buffer in the parent window and adjust its size to be slightly smaller than the parent.  in this way it is centered and is clear to the user that it is a child window of the same application

--- src/kem/mediaforeman/ui/gui_config.py
+++ src/kem/mediaforeman/ui/gui_config.py
@@ -1,9 +1,23 @@
-from Tkinter import *
+from tkinter import *
+from tkinter import ttk
 
-class MyClass(object):
+class ConfigWindow(object):
 
     def __init__(self, parent):
-        pass
-    
-    
+        self._window = Toplevel(parent)
+        self._window.wm_title("Media Foreman Configuration")
+        
+        WIN_X_BUFFER = 20
+        WIN_Y_BUFFER = 30
+        
+        self._window.geometry("%dx%d+%d+%d" % 
+                              (parent.winfo_width() - (2*WIN_X_BUFFER), 
+                               parent.winfo_height() - (2*WIN_Y_BUFFER), 
+                               parent.winfo_x() + WIN_X_BUFFER, 
+                               parent.winfo_y() + WIN_Y_BUFFER)
+        )
+        
+        '''prevent parent from being interacted with'''
+        self._window.grab_set()
+        
         
