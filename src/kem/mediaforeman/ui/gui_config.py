b4ad24fdb542fd7f564639827a1662c445e312a1
kieranemiller <kieranemiller@gmail.com>
2020-01-21 23:39:13 -0500

    add frame, scrollbar and analyses checkboxes to the GUI configuration window

--- src/kem/mediaforeman/ui/gui_config.py
+++ src/kem/mediaforeman/ui/gui_config.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from kem.mediaforeman.analyses.analysis_type import AnalysisType
 
 class ConfigWindow(object):
 
@@ -19,5 +20,41 @@ class ConfigWindow(object):
         
         '''prevent parent from being interacted with'''
         self._window.grab_set()
+        self.SetupControls()
+        self._window.focus()
         
+    def SetupControls(self):
+        self._rootFrame = self.SetupRootFrame()
+        
+        rootDirLabel = Label(self._rootFrame, text='Run These Analyses')
+        rootDirLabel.grid(row=0, column=0)
+        
+        MAX_CHECKBOXES_PER_ROW = 2
+        row = 0
+        column=0
+        
+        for analysis in AnalysisType:
+            checkbox = Checkbutton(self._rootFrame, text=analysis.name, variable=True)
+            checkbox.grid(row=row, column=column, sticky=W)
+            
+            column=column+1
+            if(column>=MAX_CHECKBOXES_PER_ROW): 
+                row = row + 1
+                column = 0
+        
+    def SetupRootFrame(self):
+        rootFrame = Frame(self._window, padx=10, pady=10)
+        rootFrame.grid(row=0,column=0, columnspan=1, rowspan=3, sticky=N+E+S+W)
+
+        Grid.rowconfigure(rootFrame, 0, weight=1)
+        Grid.columnconfigure(rootFrame, 0, weight=1)
+
+        '''
+        vsb = ttk.Scrollbar(rootFrame, orient="vertical")
+        vsb.configure(command=rootFrame.yview)
+        vsb.grid(row=0, rowspan=2, column=2, sticky=N+S)
+        rootFrame.configure(yscrollcommand=vsb.set)
+        '''
+        
+        return rootFrame
         
