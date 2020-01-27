9d1ece1fea44a55260df70eeb46d6da0cecaae5e
kieranemiller <kieranemiller@gmail.com>
2020-01-26 22:15:54 -0500

    1) create constants for default x and y padding 2) add a scrolled textbox next to the configuration button that shows the running config; this still needs formatting support as it looks hideous

--- src/kem/mediaforeman/ui/gui.py
+++ src/kem/mediaforeman/ui/gui.py
@@ -4,8 +4,12 @@ from kem.mediaforeman.analyses.analysis_type import AnalysisType
 import tkinter
 from kem.mediaforeman.ui.gui_config import ConfigWindow
 from kem.mediaforeman.app_config import AppConfig
+from tkinter.scrolledtext import ScrolledText
 
 class GuiApp(object):
+
+    PADDING_X = 5
+    PADDING_Y = 5
 
     def __init__(self):
         self._config = AppConfig()
@@ -21,19 +25,22 @@ class GuiApp(object):
         self._rootFrame = None
         self._resultsByAnalysisType = None
         
+        '''the text control that displays the running configuration'''
+        self._configText = None
+        
     def Run(self):
 
         self._rootFrame = self.SetupRootFrame()
         
         self.SetupConfigSection()
         separator = ttk.Separator(self._rootFrame, orient=HORIZONTAL)
-        separator.grid(row=2, columnspan=2, pady=5)
+        separator.grid(row=2, columnspan=2, pady=self.PADDING_Y)
         self._resultsByAnalysisType = self.SetupResultsTabs()
         
         self._window.mainloop() 
         
     def SetupRootFrame(self):
-        rootFrame = Frame(self._window, padx=10, pady=10)
+        rootFrame = Frame(self._window, padx=self.PADDING_X*2, pady=self.PADDING_Y*2)
         rootFrame.grid(row=0,column=0, columnspan=2, rowspan=3, sticky=N+E+S+W)
 
         Grid.rowconfigure(rootFrame, 3, weight=1)
@@ -43,21 +50,24 @@ class GuiApp(object):
     
     def SetupConfigSection(self):
         topFrame = Frame(self._rootFrame)
-        topFrame.grid(row=0, column=0, columnspan=2, rowspan=1, sticky=W+E)
+        topFrame.grid(row=0, column=0, columnspan=3, rowspan=1, sticky=W+E)
         Grid.columnconfigure(topFrame, 1, weight=1)
         
         rootDirLabel = Label(topFrame, text='Root Directory')
-        rootDirLabel.grid(row=0, column=0, columnspan=1, sticky=W)
+        rootDirLabel.grid(row=0, column=0, columnspan=1, sticky=W, padx=self.PADDING_X)
  
         rootDirInput = Entry(topFrame)
-        rootDirInput.grid(row=0, column=1, columnspan=2, sticky=W+E) 
+        rootDirInput.grid(row=0, column=1, columnspan=3, sticky=W+E) 
         Grid.columnconfigure(rootDirInput, 1, weight=1)
 
         processBtn = Button(topFrame, text="Process Root")
-        processBtn.grid(row=1, column=1, sticky=E)
+        processBtn.grid(row=1, column=2, sticky=E, padx=self.PADDING_X)
 
         configBtn = Button(topFrame, text="Configuration", command=self.ShowConfig)
-        configBtn.grid(row=1, column=0, sticky=W)
+        configBtn.grid(row=1, column=0, sticky=W, padx=self.PADDING_X)
+        
+        self._configText = ScrolledText(topFrame, height=3)
+        self._configText.grid(row=1, column=1, sticky=W+E)
         
     def ShowConfig(self):
         win = ConfigWindow(self, self._window)
@@ -68,6 +78,12 @@ class GuiApp(object):
         self._config.AnalysesToRun = []
         self._config.AnalysesToRun.extend(config.AnalysesToRun)
         
+        '''refresh the config display'''
+        self.UpdateConfigDisplayBox()
+        
+    def UpdateConfigDisplayBox(self):
+        self._configText.delete('1.0', END)
+        self._configText.insert('1.0', self._config.PrintConfig())
 
     def SetupResultsTabs(self):
         frame = Frame(self._rootFrame)
