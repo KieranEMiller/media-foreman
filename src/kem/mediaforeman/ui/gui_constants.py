d7fe9875cc4be76f6292f8de0be87548d8e045a8
kieranemiller <kieranemiller@gmail.com>
2020-01-27 23:16:39 -0500

    add constants for results tree column headers

--- src/kem/mediaforeman/ui/gui_constants.py
+++ src/kem/mediaforeman/ui/gui_constants.py
@@ -5,9 +5,12 @@ class GUIConstants(object):
     sometimes at different multiples, for example window padding is a 
     3*the x/y padding
     '''
-    
     PADDING_X = 5
     PADDING_Y = 5
+
+    RESULTS_TREE_COLUMN_HEADER_FILENAME = "File Name"
+    RESULTS_TREE_COLUMN_HEADER_PATH = "Path"
+    RESULTS_TREE_COLUMN_HEADER_PARENT_DIR = "Parent Dir"
 
     def __init__(self, params):
         pass
