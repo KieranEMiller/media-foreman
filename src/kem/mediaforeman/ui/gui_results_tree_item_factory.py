c6e0ac0a08cf8631dbc4a32ffe2af0d9b2e0d48c
kieranemiller <kieranemiller@gmail.com>
2020-01-27 23:26:20 -0500

    add new class that takes an analysis and generates and inserts an item into the results tree

---
+++ src/kem/mediaforeman/ui/gui_results_tree_item_factory.py
@@ -0,0 +1,35 @@
+
+class GUIResultsTreeItemFactory(object):
+
+
+    def __init__(self, params):
+        pass
+    
+    
+    def AddAnalysisToResultsTree(self, tree, analysisResult, parent = ''):
+        
+        #parentNode = tree.insert('', 'end', text='item num ' + str(i), values=('qwer', 'qwer', 'qwer'))
+        #tree.insert(parentNode, 'end', text='item num ' + str(z), values=('qwer', 'qwer', 'qwer'))
+        
+        '''the values here are the following columns '''
+        '''
+        GUIConstants.RESULTS_TREE_COLUMN_HEADER_FILENAME, 
+        GUIConstants.RESULTS_TREE_COLUMN_HEADER_PATH, 
+        GUIConstants.RESULTS_TREE_COLUMN_HEADER_PARENT_DIR
+        '''
+
+        newNode = tree.insert(
+            parent, 'end', 
+            text="{} - {}".format(
+                analysisResult.GetAnalysisType(),
+                analysisResult.Media.GetFileName()
+            ),
+            values=(
+                 analysisResult.Media.GetFileName(),
+                 analysisResult.Media.BasePath,
+                 analysisResult.Media.ParentDirectory
+            )
+        )
+        
+        return newNode
+        
