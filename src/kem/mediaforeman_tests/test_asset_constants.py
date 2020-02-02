07da25d891aa8305e928fe3d21670d44f918c651
kieranemiller <kieranemiller@gmail.com>
2020-02-02 10:41:03 -0500

    add constants file for test related assets.  currently just contains names of sample mp3 files that have specific types of issues

---
+++ src/kem/mediaforeman_tests/test_asset_constants.py
@@ -0,0 +1,13 @@
+
+class TestAssetConstants(object):
+
+    SAMPLE_MP3_COMPLETE_METADATA = "./assets/sample_mp3.mp3"
+    SAMPLE_MP3_NO_METADATA = "./assets/sample_mp3_with_no_metadata.mp3"
+    SAMPLE_MP3_172bitrate = "./assets/sample_mp3_172bitrate.mp3"
+    SAMPLE_MP3_320bitrate = "./assets/sample_mp3_320bitrate.mp3"
+
+    SAMPLE_MP3_BELOW_SIZE_REQS = "./assets/sample_mp3_with_img_below_size_minimums.mp3"
+    SAMPLE_MP3_MEETS_SIZE_REQS = "./assets/sample_mp3_with_img_meets_size_reqs_275x275.mp3"
+
+    def __init__(self, params):
+        pass
