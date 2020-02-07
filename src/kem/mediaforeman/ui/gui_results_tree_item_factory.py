191f31458351793c496389abc04443729b9652e7
kieranemiller <kieranemiller@gmail.com>
2020-02-06 23:24:43 -0500

    remove debug comments

--- src/kem/mediaforeman/ui/gui_results_tree_item_factory.py
+++ src/kem/mediaforeman/ui/gui_results_tree_item_factory.py
@@ -20,9 +20,6 @@ class GUIResultsTreeItemFactory(object):
     
     def AddAnalysisToResultsTree(self, tree, analysisResult, parent = ''):
         
-        #parentNode = tree.insert('', 'end', text='item num ' + str(i), values=('qwer', 'qwer', 'qwer'))
-        #tree.insert(parentNode, 'end', text='item num ' + str(z), values=('qwer', 'qwer', 'qwer'))
-        
         '''the values list used in tree.insert are the following columns '''
         '''
         GUIConstants.RESULTS_TREE_COLUMN_HEADER_FILENAME, 
