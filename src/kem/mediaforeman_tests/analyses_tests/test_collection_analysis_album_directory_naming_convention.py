3d0505f46d3d2d5c7afe024986d14493fd0a2f9e
kieranemiller <kieranemiller@gmail.com>
2020-02-21 20:56:46 -0500

    expand test coverage for album directory naming convention analysis to include a second good case and an incorrectly named directory case

--- src/kem/mediaforeman_tests/analyses_tests/test_collection_analysis_album_directory_naming_convention.py
+++ src/kem/mediaforeman_tests/analyses_tests/test_collection_analysis_album_directory_naming_convention.py
@@ -6,10 +6,12 @@ from kem.mediaforeman.media_collection import MediaCollection
 from kem.mediaforeman.analyses.analysis_type import AnalysisType
 from kem.mediaforeman.media_file import MediaFile
 from kem.mediaforeman.analyses.collection_analysis_album_directory_naming_convention import CollectionAnalysisAlbumDirectoryNamingConvention
+from kem.mediaforeman.analyses.analysis_issue_property_invalid import AnalysisIssuePropertyInvalid
+from kem.mediaforeman.analyses.analysis_issue_type import AnalysisIssuePropertyType
 
 class TestCollectionAnalysisAlbumDirectoryNamingConvention(TestBaseFs):
 
-    def test_album_directory_naming_convention_correct_returns_no_issues(self):
+    def test_correct_album_dir_name_returns_no_issues(self):
         
         ARTIST_NAME = "ArtistTest"
         ALBUM_NAME="Album01Test"
@@ -30,7 +32,65 @@ class TestCollectionAnalysisAlbumDirectoryNamingConvention(TestBaseFs):
         self.assertEqual(result.AnalysisType, AnalysisType.CollectionAlbumDirectoryNamingConvention)
         self.assertFalse(result.HasIssues)
 
+    def test_correct_album_dir_name_with_files_with_multiple_names_returns_no_issues(self):
+        ARTIST_NAME = "ArtistTest"
+        ALBUM_NAME="Album01Test"
+        
+        albumPath = self.CreateSubDirectory(dirName = "{} - {}".format(ARTIST_NAME, ALBUM_NAME))
 
+        '''copy files with the same name'''
+        for i in range(0, 4):
+            filePath = self.CopySampleMp3ToDir(testDir=albumPath)
+            mediaFile = MediaFile(filePath)
+            mediaFile.Album = ALBUM_NAME
+            mediaFile.AlbumArtist = ARTIST_NAME
+            mediaFile.SaveMetadata()
+
+        wrongAlbum = self.CopySampleMp3ToDir(testDir=albumPath)
+        wrongAlbum  = MediaFile(filePath)
+        wrongAlbum.Album = "qwer"
+        wrongAlbum.AlbumArtist = ARTIST_NAME
+        wrongAlbum.SaveMetadata()
+
+        wrongArtist = self.CopySampleMp3ToDir(testDir=albumPath)
+        wrongArtist  = MediaFile(filePath)
+        wrongArtist.Album = ALBUM_NAME
+        wrongArtist.AlbumArtist = "asdf"
+        wrongArtist.SaveMetadata()
+        
+        coll = MediaCollection(albumPath)
+        analysis = CollectionAnalysisAlbumDirectoryNamingConvention()
+        result = analysis.RunAnalysis(coll)
+        
+        self.assertEqual(result.AnalysisType, AnalysisType.CollectionAlbumDirectoryNamingConvention)
+        self.assertFalse(result.HasIssues)
+
+    def test_incorrect_album_portion_of_dir_name_returns_issues(self):
+        
+        ARTIST_NAME = "ArtistTest"
+        ALBUM_NAME="Album01Test"
+        
+        albumPath = self.CreateSubDirectory(dirName = "{} - {}".format(ARTIST_NAME, ALBUM_NAME))
+
+        for i in range(0, 4):
+            filePath = self.CopySampleMp3ToDir(testDir=albumPath)
+            mediaFile = MediaFile(filePath)
+            mediaFile.Album = "qwer"
+            mediaFile.AlbumArtist = ARTIST_NAME
+            mediaFile.SaveMetadata()
+
+        coll = MediaCollection(albumPath)
+        analysis = CollectionAnalysisAlbumDirectoryNamingConvention()
+        result = analysis.RunAnalysis(coll)
+        
+        self.assertTrue(result.HasIssues)
+        self.assertEqual(result.AnalysisType, AnalysisType.CollectionAlbumDirectoryNamingConvention)
+        self.assertEqual(len(result.IssuesFound), 1)
+        self.assertEqual(result.IssuesFound[0].IssueType, AnalysisIssuePropertyType.AlbumDirectoryNamingConvention)
+        self.assertEqual(result.IssuesFound[0].ExpectedVal, "{} - {}".format(ARTIST_NAME, "qwer"))
+        self.assertEqual(result.IssuesFound[0].ActualVal, "{} - {}".format(ARTIST_NAME, ALBUM_NAME))
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
