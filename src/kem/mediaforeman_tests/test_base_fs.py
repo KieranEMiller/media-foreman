7aad66e95ed046ffe7ff6ce1328e7e6169f6876d
kieranemiller <kieranemiller@gmail.com>
2020-02-22 07:46:57 -0500

    add new parameter to CopySampleMp3 that allows you to specify the destination file name; makes it easier for tests that interact with the filename

--- src/kem/mediaforeman_tests/test_base_fs.py
+++ src/kem/mediaforeman_tests/test_base_fs.py
@@ -26,12 +26,16 @@ class TestBaseFs(unittest.TestCase):
         os.mkdir(newDir)
         return newDir
 
-    def CopySampleMp3ToDir(self, testDir = None, testFile = "./assets/sample_mp3.mp3"):
+    def CopySampleMp3ToDir(self, testDir = None, testFile = "./assets/sample_mp3.mp3", destFileName = None):
         if(testDir is None):
             testDir = self._testDir
             
         pathToSample = os.path.join(os.path.dirname(os.path.realpath(__file__)), testFile)
-        dest = os.path.join(testDir, "{}.mp3".format(str(uuid.uuid1())));
+        
+        if(destFileName is None):
+            destFileName = "{}.mp3".format(str(uuid.uuid1()))
+
+        dest = os.path.join(testDir, destFileName)
         shutil.copy(os.path.abspath(pathToSample), dest)
         
         return dest
