7d6354777b0bba0627bd11bdde22eef54a00fb1b
kieranemiller <kieranemiller@gmail.com>
2020-03-15 23:18:17 -0400

    add new sample mp3 test file that has no album artist set but has an author tag

--- src/kem/mediaforeman_tests/test_asset_constants.py
+++ src/kem/mediaforeman_tests/test_asset_constants.py
@@ -5,6 +5,8 @@ class TestAssetConstants(object):
     SAMPLE_MP3_NO_METADATA = "./assets/sample_mp3_with_no_metadata.mp3"
     SAMPLE_MP3_172bitrate = "./assets/sample_mp3_172bitrate.mp3"
     SAMPLE_MP3_320bitrate = "./assets/sample_mp3_320bitrate.mp3"
+    
+    SAMPLE_MP3_NO_ALBUM_ARTIST_BUT_HAS_ARTIST = "./assets/sample_mp3_no_album_artist_but_with_artist.mp3"
 
     SAMPLE_MP3_BELOW_SIZE_REQS = "./assets/sample_mp3_with_img_below_size_minimums.mp3"
     SAMPLE_MP3_MEETS_SIZE_REQS = "./assets/sample_mp3_with_img_meets_size_reqs_275x275.mp3"
