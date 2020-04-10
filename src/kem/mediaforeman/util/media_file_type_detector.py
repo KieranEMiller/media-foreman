bf2f2d00051fb7248e24bb744e551247ae85457c
kieranemiller <kieranemiller@gmail.com>
2020-04-09 23:33:27 -0400

    update the media file type detector to use the new base class

--- src/kem/mediaforeman/util/media_file_type_detector.py
+++ src/kem/mediaforeman/util/media_file_type_detector.py
@@ -1,17 +1,11 @@
 import os
+from kem.mediaforeman.util.base_extension_detector import BaseExtensionDetector
 
-class MediaFileTypeDetector(object):
+class MediaFileTypeDetector(BaseExtensionDetector):
 
-    '''TODO: expand to include additional types.  just audio types for now'''
-    VALID_TYPES = ["mp3", "flac", "wav", "ogg", "mp4"]
-    
     def __init__(self):
         pass
         
-    def IsMediaFileType(self, path):
-        '''splitext returns a tuple filename, fileext, so index 1 
-           is the extension'''
-        extension = os.path.splitext(path)[1].lower()
-        match = [extension for extension in self.VALID_TYPES if path.endswith(extension.lower())]
-        
-        return len(match) != 0
+    def GetValidTypes(self):
+        return ["mp3", "flac", "wav", "ogg", "mp4"]
+    
