5ae8ff21353bb1e56bec77607e634a752202754c
kieranemiller <kieranemiller@gmail.com>
2020-04-09 23:32:44 -0400

    add new file type detector for related media types.  these include files like .cue, .log and .m3u which are related to media file dumps

---
+++ src/kem/mediaforeman/util/media_related_file_type_detector.py
@@ -0,0 +1,10 @@
+import os
+from kem.mediaforeman.util.base_extension_detector import BaseExtensionDetector
+
+class MediaRelatedFileTypeDetector(BaseExtensionDetector):
+
+    def __init__(self):
+        pass
+        
+    def GetValidTypes(self):
+        return [".cue", ".m3u", ".log"]
