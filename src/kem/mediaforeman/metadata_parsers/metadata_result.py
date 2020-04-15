6cca75082eabae1e6af2c4ff9bee0e5aeabd5b87
kieranemiller <kieranemiller@gmail.com>
2020-04-15 08:17:29 -0400

    add class to represent parsed metadata values and set defaults

---
+++ src/kem/mediaforeman/metadata_parsers/metadata_result.py
@@ -0,0 +1,19 @@
+
+class MetadaResult(object):
+
+    def __init__(self):
+        self.Album = ""
+        self.AlbumArtist = ""
+        self.Title = ""
+        self.TrackNumber = -1
+        
+        '''image metadata'''
+        self.CoverImgExists = False
+        self.CoverImgX = -1
+        self.CoverImgY = -1
+        
+        '''other properties '''
+        self.Duration = -1
+        self.BitRate = -1
+    
+    
