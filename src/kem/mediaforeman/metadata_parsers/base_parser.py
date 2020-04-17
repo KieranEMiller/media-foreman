84464970d595c9835a7db9efc3ff201b549b39ab
kieranemiller <kieranemiller@gmail.com>
2020-04-16 23:51:13 -0400

    add new abstract methods to the parser base class: Extract Properties and ExtractImageProperties

--- src/kem/mediaforeman/metadata_parsers/base_parser.py
+++ src/kem/mediaforeman/metadata_parsers/base_parser.py
@@ -1,6 +1,14 @@
+from abc import abstractmethod
 
 class BaseParser(object):
 
     def __init__(self, path):
         self.Path = path
         
+    @abstractmethod
+    def ExtractProperties(self):
+        pass
+    
+    @abstractmethod
+    def ExtractImageProperties(self):
+        pass
