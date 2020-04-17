4b55a4557cd7428c0be3528344c06d0aa608e396
kieranemiller <kieranemiller@gmail.com>
2020-04-17 08:06:37 -0400

    1) use the base classes method for loading the image and extract height and width 2) return a result object after parsing is complete

--- src/kem/mediaforeman/metadata_parsers/mp3parser.py
+++ src/kem/mediaforeman/metadata_parsers/mp3parser.py
@@ -1,57 +1,69 @@
 from kem.mediaforeman.metadata_parsers.base_parser import BaseParser
 import os
 import eyed3
-from PIL import Image
-from io import BytesIO
+from kem.mediaforeman.metadata_parsers.metadata_result import MetadataResult
 
 class Mp3Parser(BaseParser):
 
     def __init__(self, path):
         super(self, path).__init__(path)
         
+        '''do not load the file in the ctor, rather wait for the call to 
+        Extract Properties'''
+        self._eyed3Metadata = None
+        
     def ExtractProperties(self):
-        metadata = eyed3.load(self.BasePath)
+        self._eyed3Metadata = eyed3.load(self.Path)
         
-        if(metadata == None):
-            return
+        '''TODO: could not open or load tags, what to do here?'''
+        if(self._eyed3Metadata == None):
+            return None
 
-        if(metadata.tag != None):
-            self.Album = metadata.tag.album
-            self.Title = metadata.tag.title
+        result = MetadataResult()
+        if(self._eyed3Metadata.tag != None):
+            result.Album = self._eyed3Metadata.tag.album
+            result.Title = self._eyed3Metadata.tag.title
             
-            self.AlbumArtist = metadata.tag.album_artist
-            if(self.AlbumArtist is None or self.AlbumArtist == ""):
-                self.AlbumArtist = metadata.tag.artist
+            result.AlbumArtist = self._eyed3Metadata.tag.album_artist
+            if(result.AlbumArtist is None or self.AlbumArtist == ""):
+                result.AlbumArtist = self._eyed3Metadata.tag.artist
             
-            tracknum = metadata.tag.track_num[0]
+            tracknum = self._eyed3Metadata.tag.track_num[0]
             if(str(tracknum).isdigit()):
-                self.TrackNumber = tracknum
+                result.TrackNumber = tracknum
             
-            self.ExtractImageProperties(metadata)
+            imgResult = result.ExtractImageProperties()
+            result.CoverImgExists = imgResult.CoverImgExists
+            result.CoverImgX = imgResult.CoverImgX
+            result.CoverImgY = imgResult.CoverImgY
         
-        if(metadata.info != None):
-            self.Duration = metadata.info.time_secs
+        if(self._eyed3Metadata.info != None):
+            result.Duration = self._eyed3Metadata.info.time_secs
 
-            bitRateVrb, bitRateVal = metadata.info.bit_rate
-            self.BitRate = bitRateVal
+            bitRateVrb, bitRateVal = self._eyed3Metadata.info.bit_rate
+            result.BitRate = bitRateVal
             
-    def ExtractImageProperties(self, metadata):
+        return result
+            
+    def ExtractImageProperties(self):
         '''picture type here represents the type of image embedded in the file
              3: Front Cover, 
              4: Back Cover
              0: other
         '''
-        if(len(metadata.tag.images) > 0):
+        imgResult = MetadataResult()
+        imgResult.CoverImgExists = False
+        
+        if(len(self._eyed3Metadata.tag.images) > 0):
             '''currently only support and care about images on the front cover'''
-            frontCoverImg = list(filter(lambda i: i.picture_type == 3, metadata.tag.images))
+            frontCoverImg = list(filter(lambda i: i.picture_type == 3, self._eyed3Metadata.tag.images))
         
             if(len(frontCoverImg) > 0):
                 rawBytes =  bytearray(frontCoverImg[0].image_data)
-                img = Image.open(BytesIO(rawBytes))
-                self.CoverImgExists = True
+                width, height = self.GetImageSizeFromByteArray(rawBytes)
+                imgResult.CoverImgExists = True
+                imgResult.CoverImgX = width
+                imgResult.CoverImgY = height
+        
+        return imgResult
                 
-                width, height = img.size
-                self.CoverImgX = width
-                self.CoverImgY = height
-                
-                img.close()
