e2e9a65eae79358a903429e41c0e09b9b008ee24
kieranemiller <kieranemiller@gmail.com>
2020-04-19 16:01:17 -0400

    implement base abstract methods for the flac parser; no implementation yet

--- src/kem/mediaforeman/metadata_parsers/flacparser.py
+++ src/kem/mediaforeman/metadata_parsers/flacparser.py
@@ -1,7 +1,13 @@
-class FlacParser(object):
+from mutagen.flac import FLAC
+from kem.mediaforeman.metadata_parsers.base_parser import BaseParser
+
+class FlacParser(BaseParser):
 
     def __init__(self, params):
-        '''
-        Constructor
-        '''
-        
+        pass
+    
+    def ExtractProperties(self):
+        pass
+    
+    def ExtractImageProperties(self):
+        pass
