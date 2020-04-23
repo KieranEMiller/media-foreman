d636f674106b0310bf8c9019413a9d3de86617b1
kieranemiller <kieranemiller@gmail.com>
2020-04-23 18:30:38 -0400

    add extract image implementation and basic metadata properties for the flac parser

--- src/kem/mediaforeman/metadata_parsers/flacparser.py
+++ src/kem/mediaforeman/metadata_parsers/flacparser.py
@@ -1,6 +1,7 @@
 from mutagen.flac import FLAC
 from kem.mediaforeman.metadata_parsers.base_parser import BaseParser
 from kem.mediaforeman.metadata_parsers.metadata_result import MetadataResult
+from mutagen.id3._specs import PictureType
 
 class FlacParser(BaseParser):
 
@@ -13,10 +14,13 @@ class FlacParser(BaseParser):
         self._mutagenMeta = FLAC(self.Path)
         
         result = MetadataResult()
-        if(self._eyed3Metadata.tag != None):
-            result.Album = self._eyed3Metadata.tag.album
-            result.Title = self._eyed3Metadata.tag.title
+        if(self._mutagenMeta != None):
             
+            result.Album = self._mutagenMeta["album"][0]
+            result.Title = self._mutagenMeta["title"][0]
+            result.AlbumArtist = self._mutagenMeta["artist"][0]
+            
+            '''
             result.AlbumArtist = self._eyed3Metadata.tag.album_artist
             if(result.AlbumArtist is None or result.AlbumArtist == ""):
                 result.AlbumArtist = self._eyed3Metadata.tag.artist
@@ -24,6 +28,9 @@ class FlacParser(BaseParser):
             tracknum = self._eyed3Metadata.tag.track_num[0]
             if(str(tracknum).isdigit()):
                 result.TrackNumber = tracknum
+            '''
+                
+            result.BitRate = self._mutagenMeta.info.bitrate
             
             imgResult = self.ExtractImageProperties()
             result.CoverImgExists = imgResult.CoverImgExists
@@ -31,4 +38,13 @@ class FlacParser(BaseParser):
             result.CoverImgY = imgResult.CoverImgY
     
     def ExtractImageProperties(self):
-        pass
+        result = MetadataResult()
+        result.CoverImgExists = False
+        
+        frontImg = [pic for pic in self._mutagenMeta.pictures if pic.type == PictureType.COVER_FRONT]
+        if(frontImg != None and len(frontImg) > 0):
+            result.CoverImgExists = True
+            result.CoverImgX = frontImg[0].width
+            result.CoverImgY = frontImg[0].height
+        
+        return result
