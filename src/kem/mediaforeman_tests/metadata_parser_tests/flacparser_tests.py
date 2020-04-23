84b06cffba71d306258d44c9b78a511b6b7b4231
kieranemiller <kieranemiller@gmail.com>
2020-04-23 18:33:51 -0400

    add basic flac parsing test, still needs asserts

--- src/kem/mediaforeman_tests/metadata_parser_tests/flacparser_tests.py
+++ src/kem/mediaforeman_tests/metadata_parser_tests/flacparser_tests.py
@@ -1,9 +1,14 @@
 import unittest
+from kem.mediaforeman.metadata_parsers.flacparser import FlacParser
+from kem.mediaforeman_tests.test_base_fs import TestBaseFs
+from kem.mediaforeman_tests.test_asset_constants import TestAssetConstants
 
-class FlacParserTests(unittest.TestCase):
+class FlacParserTests(TestBaseFs):
 
-    def testName(self):
-        pass
+    def test_load_sample_flac_file(self):
+        flacMedia = self.CopySampleMp3ToDir(testFile=TestAssetConstants.SAMPLE_FLAC)
+        parser = FlacParser(flacMedia)
+        result = parser.ExtractProperties()
 
 
 if __name__ == "__main__":
