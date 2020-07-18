19b42cbd33406184e0d7618142165c94a8404836
kieranemiller <kieranemiller@gmail.com>
2020-07-18 14:22:58 -0400

    1) remove  unused imports 2) add test coverage for when the valid file name analysis fix issues is unable to correct the file name due to invalid metadata

--- src/kem/mediaforeman_tests/analyses_tests/test_file_analysis_track_naming_convention.py
+++ src/kem/mediaforeman_tests/analyses_tests/test_file_analysis_track_naming_convention.py
@@ -3,6 +3,9 @@ from kem.mediaforeman_tests.test_base_fs import TestBaseFs
 from kem.mediaforeman.media_file import MediaFile
 from kem.mediaforeman.analyses.file_analysis_track_naming_convention import FileAnalysisTrackNamingConvention
 from kem.mediaforeman.analyses.analysis_issue_type import AnalysisIssuePropertyType
+from kem.mediaforeman.analyses.analysis_fix_error import AnalysisFixError
+from kem.mediaforeman.analyses.analysis_fix_single_property import AnalysisFixSingleProperty
+import os
 
 class TestFileAnalysisTrackingNamingConvention(TestBaseFs):
 
@@ -74,6 +77,7 @@ class TestFileAnalysisTrackingNamingConvention(TestBaseFs):
         
         fixes = analysis.FixIssues(mediaFile)
         self.assertEqual(len(fixes), 1)
+        self.assertIsInstance(fixes[0], AnalysisFixSingleProperty)
         self.assertEqual(fixes[0].ChangeFrom, file)
         self.assertEqual(fixes[0].ChangeTo, mediaFile.BasePath)
         
@@ -81,6 +85,30 @@ class TestFileAnalysisTrackingNamingConvention(TestBaseFs):
         result = analysis.RunAnalysis(mediaFile)
         
         self.assertFalse(result.HasIssues)
+        
+    def test_fix_issue_returns_fix_error_when_one_or_more_metadata_fields_not_set(self):
+        file = self.CopySampleMp3ToDir(destFileName="3 - TESTTitle - testAlbum.mp3")
+
+        mediaFile = MediaFile(file)
+        mediaFile.Album="testAlbum"
+        mediaFile.Title=""
+        mediaFile.TrackNumber=3
+        mediaFile.AlbumArtist="testArtist"
+        mediaFile.SaveMetadata()
+        
+        analysis = FileAnalysisTrackNamingConvention()
+        result = analysis.RunAnalysis(mediaFile)
+        
+        self.assertTrue(result.HasIssues)
+        self.assertTrue(len(result.IssuesFound), 1)
+        self.assertEqual(result.IssuesFound[0].IssueType, AnalysisIssuePropertyType.TrackNamingConvention)
+        self.assertEqual(result.IssuesFound[0].ExpectedVal, "03 -  - testAlbum")       
+        
+        fixes = analysis.FixIssues(mediaFile)
+        self.assertEqual(len(fixes), 1)
+        self.assertIsInstance(fixes[0], AnalysisFixError)
+        
+        self.assertTrue(os.path.isfile(file))
 
         
 if __name__ == "__main__":
